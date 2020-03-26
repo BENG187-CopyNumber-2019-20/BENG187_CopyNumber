6a47a05aa3c096db67e29bed5fb4b82b9f56c055
Ariel Schreiman <aschreim@ucsd.edu>
2020-03-26 00:10:12 -0700

    More bugfixes. Submitted jobs to TSCC, will see how those go

--- pipeline.py
+++ pipeline.py
@@ -24,67 +24,67 @@ def create_sample(line, directory):
          return None #if the line isn't a sample we are going to use either because it is wgs or already in bam format or in the wrong format, don't make an object
 
 def write_pbs_file(output_dir, sample, genome, username, current_dir): #TODO add timing commands to this
-   file = open(output_dir + '/' + sample.name + '_pipeline.pbs')
+   file = open(output_dir + '/' + sample.name + '_pipeline.pbs', 'w')
 
    #default .pbs job header
-   file.write('#!/bin/bash')
-   file.write('#PBS -q hotel')
-   file.write('#PBS -l nodes=1:ppn=8')
-   file.write('#PBS -l walltime=20:00:00')
-   file.write('#PBS -m bea')
-   file.write('#PBS -M ' + username + '@ucsd.edu')  #assuming for now that email is of format username@ucsd.edu
-   file.write('#PBS -o ' + sample.name + '_pipeline.o')
-   file.write('#PBS -e ' + sample.name + '_pipeline.e')
-   file.write('#PBS -N ' + sample.name + '_pipeline_' + username)
-   file.write('#PBS -V')
-   file.write('')
+   file.write('#!/bin/bash\n')
+   file.write('#PBS -q hotel\n')
+   file.write('#PBS -l nodes=1:ppn=8\n')
+   file.write('#PBS -l walltime=20:00:00\n')
+   file.write('#PBS -m bea\n')
+   file.write('#PBS -M ' + username + '@ucsd.edu\n')  #assuming for now that email is of format username@ucsd.edu
+   file.write('#PBS -o ' + sample.name + '_pipeline.o\n')
+   file.write('#PBS -e ' + sample.name + '_pipeline.e\n')
+   file.write('#PBS -N ' + sample.name + '_pipeline_' + username + '\n')
+   file.write('#PBS -V\n')
+   file.write('\n')
 
    #need to load R in the job for DNAcopy to work
-   file.write('module load R')
-   file.write('')
+   file.write('module load R\n')
+   file.write('\n')
 
    #fastqc
-   file.write('fastqc' + sample.directory + '/' + sample.tumor + '_1.fastq.gz -o ' + output_dir)
-   file.write('fastqc' + sample.directory + '/' + sample.tumor + '_2.fastq.gz -o ' + output_dir)
-   file.write('fastqc' + sample.directory + '/' + sample.normal + '_1.fastq.gz -o ' + output_dir)
-   file.write('fastqc' + sample.directory + '/' + sample.normal + '_2.fastq.gz -o ' + output_dir)
-   file.write('')
+   file.write('fastqc ' + sample.directory + '/' + sample.tumor + '_1.fastq.gz -o ' + output_dir + '\n')
+   file.write('fastqc ' + sample.directory + '/' + sample.tumor + '_2.fastq.gz -o ' + output_dir + '\n')
+   file.write('fastqc ' + sample.directory + '/' + sample.normal + '_1.fastq.gz -o ' + output_dir + '\n')
+   file.write('fastqc ' + sample.directory + '/' + sample.normal + '_2.fastq.gz -o ' + output_dir + '\n')
+   file.write('\n')
 
    #TODO add QC check
 
    #bwa for tumor
-   file.write('bwa mem -t 8 -k 35 ' + genome + ' ' + sample.directory + '/' + sample.tumor + '_1.fastq.gz ' + sample.directory + '/' + sample.tumor + '_2.fastq.gz | samtools sort -@ 8 -o ' + output_dir + '/' + sample.name + '_tumor.bam -')
-   file.write('/opt/biotools/GenomeAnalysisTK/4.0.11.0/gatk MarkDuplicates -I ' + output_dir + '/' + sample.name + '_tumor.bam -O ' + output_dir + '/' + sample.name + '_tumor_md.bam -M ' + output_dir + '/' + sample.name + '_tumor_md.metrics --REMOVE_DUPLICATES')
-   file.write('samtools index ' + output_dir + '/' + sample.name + '_tumor_md.bam')
-   file.write('')
+   file.write('bwa mem -t 8 -k 35 ' + genome + ' ' + sample.directory + '/' + sample.tumor + '_1.fastq.gz ' + sample.directory + '/' + sample.tumor + '_2.fastq.gz | samtools sort -@ 8 -o ' + output_dir + '/' + sample.name + '_tumor.bam -\n')
+   file.write('/opt/biotools/GenomeAnalysisTK/4.0.11.0/gatk MarkDuplicates -I ' + output_dir + '/' + sample.name + '_tumor.bam -O ' + output_dir + '/' + sample.name + '_tumor_md.bam -M ' + output_dir + '/' + sample.name + '_tumor_md.metrics --REMOVE_DUPLICATES\n')
+   file.write('samtools index ' + output_dir + '/' + sample.name + '_tumor_md.bam\n')
+   file.write('\n')
 
    #bwa for normal
-   file.write('bwa mem -t 8 -k 35 ' + genome + ' ' + sample.directory + '/' + sample.normal + '_1.fastq.gz ' + sample.directory + '/' + sample.normal + '_2.fastq.gz | samtools sort -@ 8 -o ' + output_dir + '/' + sample.name + '_normal.bam -')
-   file.write('/opt/biotools/GenomeAnalysisTK/4.0.11.0/gatk MarkDuplicates -I ' + output_dir + '/' + sample.name + '_normal.bam -O ' + output_dir + '/' + sample.name + '_normal_md.bam -M ' + output_dir + '/' + sample.name + '_normal_md.metrics --REMOVE_DUPLICATES')
-   file.write('samtools index ' + output_dir + '/' + sample.name + '_normal_md.bam')
-   file.write('')
+   file.write('bwa mem -t 8 -k 35 ' + genome + ' ' + sample.directory + '/' + sample.normal + '_1.fastq.gz ' + sample.directory + '/' + sample.normal + '_2.fastq.gz | samtools sort -@ 8 -o ' + output_dir + '/' + sample.name + '_normal.bam -\n')
+   file.write('/opt/biotools/GenomeAnalysisTK/4.0.11.0/gatk MarkDuplicates -I ' + output_dir + '/' + sample.name + '_normal.bam -O ' + output_dir + '/' + sample.name + '_normal_md.bam -M ' + output_dir + '/' + sample.name + '_normal_md.metrics --REMOVE_DUPLICATES\n')
+   file.write('samtools index ' + output_dir + '/' + sample.name + '_normal_md.bam\n')
+   file.write('\n')
 
    #mpileups
-   file.write('samtools mpileup -f ' + genome + ' -q 5 -Q 0 -d 50000 -B -o ' + output_dir + '/' + sample.name + '_tumor.vcf ' + output_dir + '/' + sample.name + '_tumor_md.bam')
-   file.write('samtools mpileup -f ' + genome + ' -q 5 -Q 0 -d 50000 -B -o ' + output_dir + '/' + sample.name + '_normal.vcf ' + output_dir + '/' + sample.name + '_normal_md.bam')
-   file.write('')
+   file.write('samtools mpileup -f ' + genome + ' -q 5 -Q 0 -d 50000 -B -o ' + output_dir + '/' + sample.name + '_tumor.vcf ' + output_dir + '/' + sample.name + '_tumor_md.bam\n')
+   file.write('samtools mpileup -f ' + genome + ' -q 5 -Q 0 -d 50000 -B -o ' + output_dir + '/' + sample.name + '_normal.vcf ' + output_dir + '/' + sample.name + '_normal_md.bam\n')
+   file.write('\n')
 
    #generate large segments for amplification and deletion calling
-   file.write('varscan copynumber ' + output_dir + '/' + sample.name + '_normal.vcf ' + output_dir + '/' + sample.name + '_tumor.vcf ' + output_dir + '/' + sample.name + '_varscan --min-base-qual 0 --min-map-qual 0 --min-segment-size 50 --max-segment-size 1000')
-   file.write('varscan copyCaller ' + output_dir + '/' + sample.name + '_varscan.copynumber --output-file ' + output_dir + '/' + sample.name + '_varscan.copynumber.called')
-   file.write('Rscript --vanilla ' + current_dir + '/DNAcopy.R ' + output_dir + '/' + sample.name + '_varscan.copynumber.called')
-   file.write('')
+   file.write('varscan copynumber ' + output_dir + '/' + sample.name + '_normal.vcf ' + output_dir + '/' + sample.name + '_tumor.vcf ' + output_dir + '/' + sample.name + '_varscan --min-base-qual 0 --min-map-qual 0 --min-segment-size 50 --max-segment-size 1000\n')
+   file.write('varscan copyCaller ' + output_dir + '/' + sample.name + '_varscan.copynumber --output-file ' + output_dir + '/' + sample.name + '_varscan.copynumber.called\n')
+   file.write('Rscript --vanilla ' + current_dir + '/DNAcopy.R ' + output_dir + '/' + sample.name + '_varscan.copynumber.called\n')
+   file.write('\n')
    
    #generate short segments for plotting so we don't have to plot every single point
-   file.write('varscan copynumber ' + output_dir + '/' + sample.name + '_normal.vcf ' + output_dir + '/' + sample.name + '_tumor.vcf ' + output_dir + '/' + sample.name + '_varscan_plotting --min-base-qual 0 --min-map-qual 0 --min-segment-size 5 --max-segment-size 50')
-   file.write('varscan copyCaller ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber --output-file ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber.called')
-   file.write('Rscript --vanilla ' + current_dir + '/DNAcopy.R ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber.called')
-   file.write('')
+   file.write('varscan copynumber ' + output_dir + '/' + sample.name + '_normal.vcf ' + output_dir + '/' + sample.name + '_tumor.vcf ' + output_dir + '/' + sample.name + '_varscan_plotting --min-base-qual 0 --min-map-qual 0 --min-segment-size 5 --max-segment-size 50\n')
+   file.write('varscan copyCaller ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber --output-file ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber.called\n')
+   file.write('Rscript --vanilla ' + current_dir + '/DNAcopy.R ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber.called\n')
+   file.write('\n')
 
    #generate a plot of the sample highlighting regions of amplification and deletion
    #TODO write this python file to make a plot using numpy similar to how we've made plots in matlab
-   #file.write('python sample_visualization.py ' + output_dir + '/' + sample.name + '_varscan.copynumber.called.segmentation ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber.called.segmentation')
-   #file.write('')
+   #file.write('python sample_visualization.py ' + output_dir + '/' + sample.name + '_varscan.copynumber.called.segmentation ' + output_dir + '/' + sample.name + '_varscan_plotting.copynumber.called.segmentation\n')
+   #file.write('\n')
 
    file.close()
 
@@ -152,7 +152,7 @@ def main(argv):
       call(['mkdir', '-p', sample_dir])
       call(['mkdir', '-p', sample_dir + '/source'])
       write_pbs_file(sample_dir + '/source', sample, genome, username, current_dir) #assuming for now that email is of format username@ucsd.edu
-      #call(['qsub', sample_dir + '/source/' + sample.name + '_pipeline.pbs'])
+      call(['qsub', sample_dir + '/source/' + sample.name + '_pipeline.pbs'])
 
 
 
